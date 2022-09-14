7baa64ee11a4c751e01ec9729bbd1bef35743534
MJ <mahmad2@mix.wvu.edu>
2022-09-14 15:03:29 -0400

    added CircleProgram.py and CircleScreen.py

---
+++ CircleProgram.py
@@ -0,0 +1,35 @@
+"""
+This program calculates different things related to circles
+"""
+# Import the library mth, we need it to obtain the value of PI
+import math
+
+
+def CircleArea(diameter):
+    """
+    This method calculates the area of a circle, given the circle's diameter
+    :param diameter: passed value of the circle's diameter
+    :return: the area of the circle
+    """
+    # The area is radius (square) * pie
+    # Let us calculate radius:
+    radius = 0.5 * diameter
+
+    # Let us calculate area
+    area = radius * math.pi
+    return area
+
+
+def ShowCircleArea():
+    """
+    This method calculates the area of a circle, by asking use for a diameter
+    :return: N/A
+    """
+    diameter = float(input("What is the diameter of the cirlce?"))
+    # The area is radius (square) * pie
+    # Let us calculate radius:
+    radius = 0.5 * diameter
+
+    # Let us calculate area
+    area = radius * math.pi
+    print("The area of a circle of {0}".format(diameter), " is {0}".format(area))
---
+++ CircleScreen.py
@@ -0,0 +1,23 @@
+"""
+This program uses the methods defined in the CircleScreen file
+"""
+# import the  file which has the functions we want to use
+import CircleProgram
+
+
+# define teh main method. Just use this as is.
+def main():
+    # Your code goes here.
+    # define diameter
+    theDiameter = 10.9
+
+    # pass the diameter to the method, call the method, and store the returned value in the theArea variable
+    theArea = CircleProgram.CircleArea(theDiameter)
+
+    # print out the output
+    print("The area of a circle of {0}".format(theDiameter), " is {0}".format(theArea))
+
+    # Call the method which shows the area
+    CircleProgram.ShowCircleArea()
+if __name__ == "__main__":
+    main()
