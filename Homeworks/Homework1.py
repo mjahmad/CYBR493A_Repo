e389754a8e0b7103b96f1ec14d6ed18dee9c4b15
MJ <mahmad2@mix.wvu.edu>
2022-09-28 15:16:06 -0400

    Added Answer key to HW #1

--- Homeworks/Homework1.py
+++ Homeworks/Homework1.py
@@ -18,15 +18,12 @@ def CorrectPasswordResponse(password):
 
     # If the password's length is < 8, or has no digit or has no symbols, then return False
     if len(password) < 8:
-        print("Length")
         return False
     # if no digit is found, then return False
     elif not any(char.isdigit() for char in password):
-        print("Number")
         return False
     # If no symbols found, return False
     elif not any(char in symbols for char in password):
-        print("Symbols")
         return False
     # Return True if all IF statements fail (do not return anything)
     return True
