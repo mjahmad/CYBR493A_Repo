52fe7dc0b77513b9640b627a7b17ad776f758424
MJ <mahmad2@mix.wvu.edu>
2022-10-03 14:13:26 -0400

    Uploaded Web scraping files under In-Class-Practices

---
+++ In_Class_Practices/Web_Scrapping_Main.py
@@ -0,0 +1,25 @@
+import Web_Scrapping as wb
+
+def main():
+    """
+    Main screen for the web scrapping file
+    :return:
+    """
+    main_tree = wb.get_web_tree("https://www.wvu.edu")
+    print(main_tree.xpath)
+    # Find any tag with the id wvu-main, then go down one level, and obtain whatever text there is.
+    # Right-click on your browser and hit Inspect (Chrome is preferable)
+    uni_name = main_tree.xpath('//*[@id="wvu-main"]/h1/text()')
+    #relationships = main_tree.xpath('//*[@id="oc_888_Relationships"]/div/div/div/div/div/text()')
+    print(((uni_name)))
+    # Let us see how many divs are they under this section
+    all_divs = main_tree.xpath('//*[@id="wvu-main"]/div')
+    print(str(len(all_divs)))
+    # Interact with the first div
+    first_div = all_divs[0]
+    print(str(len(first_div)))
+
+if __name__ == "__main__":
+    main()
+
+
