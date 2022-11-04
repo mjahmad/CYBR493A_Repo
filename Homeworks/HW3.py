6ea2fdf51b6fd8da8efb2c504f419dea79e8fc70
MJ <mahmad2@mix.wvu.edu>
2022-11-04 14:14:40 -0400

    Added HW #3 files.

---
+++ Homeworks/HW3.py
@@ -0,0 +1,71 @@
+from In_Class_Practices import Web_Scraping as wb
+import re
+"""
+Homework #3 objective is to fill out and complete each of the following methods.    
+    """
+
+def Generate_Links_For_All_Bugs_Pages(link):
+    """
+        30 POINTS
+       A method which creates an entire list of URLs of all bug pages of the initial link
+       :param link: link to the initial launchpad vulnerabilities (page one) passed from main
+       :return: list of URLs, each to a page of bugs
+    """
+    # Empty list, add all urls to this list
+    bug_pages_list = []
+
+    # Each time you create a new link, store it in this variable
+    current_link = ''
+
+    # add the current link to the list
+    bug_pages_list.append(current_link)
+
+    return bug_pages_list
+
+
+def GetBugIDs(page_link):
+    """
+        A method which iterates through the entire list of bug reports of any  given link
+        :param link: link to launchpad bug report page passed from main
+        :return: list of bug IDs of the page_link page
+    """
+    # Empty list, add all bug IDs to this list
+    bug_list=[]
+
+    return bug_list
+
+
+def GetBugPackages(page_link):
+    """
+        A method which iterates through the entire list of bug reports of teh given link
+        :param link: link to launchpad bug reports passed from main
+        :return: list of bug packages of the page_link page
+    """
+    # Empty list, add all bug packages to this list
+    bug_package = []
+    return bug_package
+
+
+def GetTotalNumberOfBugs(page_link):
+    """
+        A method which gets extracts total number of bugs from the initial link of Ubuntu Vulnerabilities
+        :param link: link to launchpad bug reports page
+        :return: the total number of bugs from the first page
+    """
+    # Extract and clean total number of bugs
+
+    noOfBugs = 0
+    return noOfBugs
+
+
+def GetTotalNumberOfPages(page_link):
+    """
+            A method which calculates the total number of bug pages from the initial link of Ubuntu Vulnerabilities
+            :param link: link to launchpad bug reports page
+            :return: the total number of bug pages
+        """
+
+    # Extract and clean total number of bugs
+    GetTotalNumberOfBugs(page_link)
+    noOfPages = 0
+    return noOfPages
---
+++ Homeworks/HW3_Main_Screen.py
@@ -0,0 +1,52 @@
+"""
+This is the main screen for Homework #3. Do not make any changes to this file.
+"""
+import HW3
+# import the necessary files
+from Homeworks import HW3org
+
+# Global variables, accessed by all method in this file
+link = link = "https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist" \
+              "=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED" \
+              "&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field" \
+              ".importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance" \
+              "%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type" \
+              "%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA" \
+              "&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field" \
+              ".structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field" \
+              ".status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field" \
+              ".affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field" \
+              ".has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field" \
+              ".has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby" \
+              "=-importance&memo=75&start=0 "
+
+# Get links to all bug pages of Ubuntu Vulnerabilities
+# Also Global
+bugsLinks_list = HW3.Generate_Links_For_All_Bugs_Pages(link)
+
+
+def main():
+    """
+    Main screen for HW #3.
+    :return:N/A
+    """
+    # Display initial info
+    bugsTotal = HW3.GetTotalNumberOfBugs(link)
+    pagesTotal = HW3.GetTotalNumberOfPages(link)
+    print('Welcome! Infor about this Vulnerabilities DB')
+    print('Initial Link:\n' + link)
+    print('Total Number of Bugs:\t', bugsTotal)
+    print('Total Number of Pages:\t', pagesTotal)
+
+    # Accept page number form user
+    page_input = int(input(('Which page would you like to view? Enter a number between 0 and ', pagesTotal - 1)))
+
+    # Get data for user's input page
+    print('Page # ', page_input, ' information:\t')
+    print('Page Link:\t' + bugsLinks_list[page_input])
+    print('List of Bugs\t', HW3.GetBugIDs(bugsLinks_list[page_input]))
+    print('List of Packages\t', HW3.GetBugPackages(bugsLinks_list[page_input]))
+
+
+if __name__ == "__main__":
+    main()
